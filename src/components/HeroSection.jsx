51696d11393ca1cb900347e4aaf7e9df901b3460
Linda <linda.bertling@utb.ecutbildning.se>
2023-07-26 14:59:10 +0200

    updated styling for hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,58 +1,51 @@
 import React from "react";
 import Image from "next/image";
-import classes from "./heroSection.module.css";
+import Link from "next/link";
 
-import heroImage from "../../public/images/img_hero-image.jpg";
-import profilePicture from "../../public/images/img_profile-picture.jpg";
+import classes from "./heroSection.module.css";
+import { mukta, sourceCodePro } from "../utils/fonts";
+
+import profilePicture from "../../public/icons/icon_avatar.svg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
-import Link from "next/link";
 // import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const HeroSection = () => {
   return (
     <div className={classes.heroContentWrapper}>
-      <Image
-        src={heroImage}
-        alt="photo of purple paint"
-        className={classes.heroImage}
-      />
       <section className={classes.aboutSection}>
-        <div className={classes.introduction}>
+        <div className={classes.avatarContainer}>
           <Image
             src={profilePicture}
-            alt="photo of Linda"
+            alt="Illustration of coder"
             className={classes.profilePicture}
           />
-          <div>
-            <h2>Linda Bertling</h2>
-            <h1>Frontend Developer</h1>
-            <section className={classes.heroIcons}>
-              <Link href={"https://www.linkedin.com/in/linda-bertling/"}>
-                <FontAwesomeIcon
-                  icon={faLinkedinIn}
-                  size="2xl"
-                  style={{ color: "#3c1d5a" }}
-                />
-              </Link>
-              <Link href={"https://github.com/lindabertling?tab=repositories"}>
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  size="2xl"
-                  style={{ color: "#3c1d5a" }}
-                />
-              </Link>
-            </section>
-          </div>
         </div>
-        <div className={classes.aboutDescription}>
-          <p>
+        <div className={classes.introduction}>
+          <h2 className={mukta.className}>Hi, I'm Linda.</h2>
+          <p className={sourceCodePro.className}>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Voluptates, recusandae. Consequatur eligendi, totam ad voluptas
             cupiditate sequi in ipsam impedit velit repellat dicta recusandae
             fugiat autem inventore qui corrupti nisi!
           </p>
+          <section className={classes.heroIcons}>
+            <Link href={"https://www.linkedin.com/in/linda-bertling/"}>
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                size="2xl"
+                style={{ color: "#FFF" }}
+              />
+            </Link>
+            <Link href={"https://github.com/lindabertling?tab=repositories"}>
+              <FontAwesomeIcon
+                icon={faGithub}
+                size="2xl"
+                style={{ color: "#FFF" }}
+              />
+            </Link>
+          </section>
         </div>
       </section>
     </div>
