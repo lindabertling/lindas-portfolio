4cb831f5f65a56fdd5981a5bc7505b788920d90e
Linda <linda.bertling@utb.ecutbildning.se>
2023-08-10 19:51:59 +0200

    working on media query for navigation + adding menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,28 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUp } from "@fortawesome/free-solid-svg-icons";
 
+const HamburgerMenu = ({ isMenuOpen, handleClose }) => {
+  return (
+    <div
+      className={`${classes.hamburgerMenu} ${isMenuOpen ? classes.open : ""}`}
+      onClick={handleClose}
+    >
+      <div className={classes.bar}></div>
+      <div className={classes.bar}></div>
+      <div className={classes.bar}></div>
+    </div>
+  );
+};
+
 const Navbar = () => {
   const [showBackToTopIcon, setShowBackToTopIcon] = useState(false);
+  const [viewportWidth, setViewportWidth] = useState();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
+    setViewportWidth(window.innerWidth);
+    console.log(viewportWidth);
+
     const handleScroll = () => {
       const breakPoint = 600;
       const windowScroll = window.scrollY;
@@ -23,16 +41,24 @@ const Navbar = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [viewportWidth]);
+
+  const handleClose = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <>
-      <div className={classes.navbarContainer}>
-        <a href="/#about">About</a>
-        <a href="/#projects">Projects</a>
-        <a href="/#skills">Skills</a>
-        <a href="/#contact">Contact</a>
-      </div>
+      {viewportWidth > 600 ? (
+        <div className={classes.navbarContainer}>
+          <a href="/#about">About</a>
+          <a href="/#projects">Projects</a>
+          <a href="/#skills">Skills</a>
+          <a href="/#contact">Contact</a>
+        </div>
+      ) : (
+        <HamburgerMenu isMenuOpen={isMenuOpen} handleClose={handleClose} />
+      )}
       {showBackToTopIcon && (
         <div className={classes.backToTopIcon}>
           <Link href={"/#"}>
